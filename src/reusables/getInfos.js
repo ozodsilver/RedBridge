9ed826a27dbff0427feb919e1ea6d47a4580e110
ozodsilver <ozodsilver@gmail.com>
2023-01-03 19:53:52 +0500

    https da muammo

--- src/reusables/getInfos.js
+++ src/reusables/getInfos.js
@@ -1,4 +1,4 @@
 // http://84.54.115.45:5123/
 // let  base = 'http://localhost:5123/api/'
-let  base = 'https://84.54.115.45:5123/api/'
+let  base = 'http://84.54.115.45:5123/api/'
 export default base;
