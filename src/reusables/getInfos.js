f20d6c2dad76daf407b1244364b8ec3ac7d3b8d8
ozodsilver <ozodsilver@gmail.com>
2023-02-03 00:46:01 +0500

    protokol o'zgardi

--- src/reusables/getInfos.js
+++ src/reusables/getInfos.js
@@ -1,5 +1,5 @@
 // http://84.54.115.45:5123/
 // let  base = 'http://localhost:5123/api/'
-let base  = 'http://84.54.115.45:5123/api/'
+let base  = 'https://84.54.115.45:5123/api/'
 // let  base = 'https://rb.algorithmic.uz/api/'
 export default base;
