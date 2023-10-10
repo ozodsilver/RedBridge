d255cf57d8b7115240bb77fcb7bec3da19a4fd05
ozodsilver <ozodsilver@gmail.com>
2023-10-10 14:51:28 +0500

    base changed

--- src/reusables/getInfos.js
+++ src/reusables/getInfos.js
@@ -1,5 +1,5 @@
 // http://84.54.115.45:5123/
-let  base = 'https://rb.algorithmic.uz/api/'
-// let base  = 'http://84.54.115.45:5123/api/'
+// let  base = 'https://rb.algorithmic.uz/api/'
+let base  = 'http://192.168.1.158:5000/api/'
 // let  base = 'https://28a2-213-230-93-31.ngrok-free.app/api/'
 export default base;
