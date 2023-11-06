e4c9d4fefcafb9dc29db1fb1b34dbc61e9e60708
ozodsilver <ozodsilver@gmail.com>
2023-11-06 21:12:41 +0500

    DailyProccess added

--- src/components/History/components/historyDate.vue
+++ src/components/History/components/historyDate.vue
@@ -1,18 +1,36 @@
 <template>
     <div>
-      
+
+
+      <div v-if="customers.length == 0">
+        <n-result
+    status="warning"
+    title="Warning"
+    description="No information found"
+    class="absolute left-2/4 top-2/4 "
+  >
+
+  </n-result>
+      </div>
+
+
+
+      <div  v-if="customers.length !== 0">
+        <h1 class="m-3 text-2xl">Date</h1>
         <DataTable :value="customers" paginator :rows="10" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem">
           
-            <Column  style="width: 100%">
-            <template #body = 'slotProps'>
-<router-link :to="{name:'historySubjects', params:{id:slotProps.data.id}}">
-  
-    {{  new Date(slotProps.data.date).toLocaleDateString() }}
+          <Column  style="width: 100%">
+          <template #body = 'slotProps'>
+<router-link :to="{name:'historySubjects', query:{id:route.params.id, date:slotProps.data.date}}" class="bg-slate-200 block w-full p-2 rounded-lg px-4">
+
+  {{  new Date(slotProps.data.date).toLocaleDateString() }}
 </router-link>
-            </template>
-            </Column>
-         
-        </DataTable>
+          </template>
+          </Column>
+       
+      </DataTable>
+      </div>
+     
     
     </div>
 </template>
@@ -25,6 +43,8 @@ import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import ColumnGroup from 'primevue/columngroup';   // optional
 import Row from 'primevue/row';                   // optional
+
+
 
 import {useRouter, useRoute} from 'vue-router'
 
@@ -48,6 +68,12 @@ onMounted(()=>{
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+:global(tr){
+background: transparent !important;
+}
 
+:global(td){
+background: transparent !important;
+}
 </style>
--- src/components/History/components/insideHistorySubjects.vue
+++ src/components/History/components/insideHistorySubjects.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <DataTable :value="products" tableStyle="min-width: 50rem">
-            <Column field="code" header="Code"></Column>
-            <Column field="name" header="Name"></Column>
-            <Column field="category" header="Category"></Column>
-            <Column field="quantity" header="Quantity"></Column>
-        </DataTable>
+  
+<DataTable :value="products" tableStyle="min-width: 50rem">
+    <Column field="code" header="Code"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="category" header="Category"></Column>
+    <Column field="quantity" header="Quantity"></Column>
+</DataTable>
     </div>
 </template>
 
@@ -17,12 +18,21 @@ import Row from 'primevue/row';                   // optional
 import {ref, onMounted} from 'vue'
 import axios from 'axios';
 
+import {useRoute} from 'vue-router';
+const route = useRoute()
 
 const subjects = ref([])
 
 onMounted(()=>{
 
-    // axios.get()
+    axios.get(`https://rb.algorithmic.uz/api/DailyProccess/history/${route.query.id}?date=${route.query.date}&zoneInfo=UTC%2B5`,  {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      }).then(res =>{
+console.log(res.data);
+})
 
 })
 
